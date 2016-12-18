6751e70e0da76da4714f850bb3387b6fa5fc2a57
mcrocks999 <paulmakles@gmail.com>
2016-12-18 17:26:02 +0000

    Project setup

---
+++ src/main/java/tech/tabpixels/pixelmod/mini/App.java
@@ -0,0 +1,13 @@
+package tech.tabpixels.pixelmod.mini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
