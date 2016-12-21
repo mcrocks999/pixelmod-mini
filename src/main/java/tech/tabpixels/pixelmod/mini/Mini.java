2fdcdf67fac597b3658c3a777dec7d4d7582cbf9
mcrocks999 <paulmakles@gmail.com>
2016-12-21 18:10:53 +0000

    Technic support added, general bug fixes as well.

--- src/main/java/tech/tabpixels/pixelmod/mini/Mini.java
+++ src/main/java/tech/tabpixels/pixelmod/mini/Mini.java
@@ -1,7 +1,6 @@
 package tech.tabpixels.pixelmod.mini;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,44 +10,61 @@ import javax.swing.JOptionPane;
 
 import java.awt.Font;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFileChooser;
 
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class Mini {
 
-	public static JFrame frmMiniInstaller;
+	public static JFrame frame;
 	public static JPanel contentPane;
 	private static JTextField textField;
+	public static JComboBox comboBox = new JComboBox();
+    public static JComboBox comboBox_1 = new JComboBox();
+	public static String workingDirectory = "";
+	public static Boolean isSelected = false;
+	public static Boolean isValid = false;
 	
 	/**
 	 * @wbp.parser.entryPoint
 	 */
 	public static void create() {
-		frmMiniInstaller = new JFrame();
-		frmMiniInstaller.setTitle("Mini Installer");
-		frmMiniInstaller.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmMiniInstaller.setBounds(100, 100, 450, 128);
+		frame = new JFrame();
+		frame.setTitle("Mini Installer");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 450, 145);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		contentPane.setLayout(new BorderLayout(0, 0));
-		frmMiniInstaller.setContentPane(contentPane);
+		frame.setContentPane(contentPane);
 		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		new  FileDrop( panel, new FileDrop.Listener() {
 			public void  filesDropped( java.io.File[] files ) {
-				for (File file : files)
-				{
-					try {
-							Files.copy(file.toPath(), new File(textField.getText()+"/mods/"+file.getName()).toPath());
-					} catch (IOException e1) {
-						JOptionPane.showMessageDialog(null, "Sorry! An error occured while copying the mod, '"+file.getName()+"'");
-						e1.printStackTrace();
+				if (isSelected) {
+					for (File file : files)
+					{
+						try {
+								Files.copy(file.toPath(), new File(textField.getText()+"/"+file.getName()).toPath());
+						} catch (IOException e1) {
+							JOptionPane.showMessageDialog(null, "Sorry! An error occured while copying the mod, '"+file.getName()+"'");
+							e1.printStackTrace();
+						}
 					}
+				} else {
+					JOptionPane.showMessageDialog(null, "A installation / modpack / instance is not selected properly.");
 				}
 			}
 	      });
@@ -69,7 +85,6 @@ public class Mini {
 		panel.add(textField);
 		textField.setColumns(10);
 		
-		String workingDirectory = "";
 		String OS = (System.getProperty("os.name")).toUpperCase();
 		if (OS.contains("WIN")) {
 		    workingDirectory = System.getenv("AppData");
@@ -77,8 +92,111 @@ public class Mini {
 		    workingDirectory = System.getProperty("user.home");
 		    workingDirectory += "/Library/Application Support";
 		}
-		textField.setText(workingDirectory+"/.minecraft");
+        Settings.technicDirectory = workingDirectory+"/.technic/modpacks";
+        Settings.minecraftDirectory = workingDirectory+"/.minecraft/mods";
 		
-		frmMiniInstaller.setVisible(true);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Default Minecraft", "Technic Modpack", "MultiMC"}));
+		comboBox.setBounds(10, 79, 156, 20);
+		panel.add(comboBox);
+		
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {".minecraft"}));
+		comboBox_1.setBounds(176, 79, 248, 20);
+		panel.add(comboBox_1);
+		
+		comboBox.addActionListener (new ActionListener () {
+		    public void actionPerformed(ActionEvent e) {
+		        changeMode(-1);
+		    }
+		});
+		comboBox_1.addActionListener (new ActionListener () {
+		    public void actionPerformed(ActionEvent e) {
+		        changeInstance();
+		    }
+		});
+		
+		frame.setVisible(true);
+		String[] options = new String[] {"Minecraft", "Technic", "MultiMC"};
+	    int response = JOptionPane.showOptionDialog(null, "Please select an installation", "PixelMod Mini",
+	        JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+	        null, options, options[0]);
+	    
+	    changeMode(response);
 	}
+    
+	public static void changeMode(int mode)
+    {
+		if (mode>-1) {comboBox.setSelectedIndex(mode);}
+		
+		isSelected = false;
+		isValid = false;
+        if (comboBox.getSelectedIndex()==0) {
+        	Settings.selection = 0;
+        	loadMinecraft();
+        } else if (comboBox.getSelectedIndex()==1) {
+        	Settings.selection = 1;
+        	loadTechnic();
+        } else {
+        	Settings.selection = 2;
+        	textField.setText("NO CURRENT SUPPORT FOR MULTIMC");
+        }
+    }
+	
+	public static void changeInstance()
+    {
+		if (isValid) {
+			if (Settings.selection == 1) {
+				textField.setText(Settings.technicDirectory + "/"+comboBox_1.getItemAt(comboBox_1.getSelectedIndex())+"/mods");
+				isSelected = true;
+	        }
+		} else if (Settings.selection != 0) {
+			JOptionPane.showMessageDialog(null, "Installation is not valid.");
+		}
+    }
+    
+    public static void loadMinecraft()
+    {
+        comboBox_1.setModel(new DefaultComboBoxModel(new String[] {".minecraft"}));
+    	if (Files.exists(Paths.get(Settings.minecraftDirectory))) {
+		    comboBox_1.setModel(new DefaultComboBoxModel(new String[]{"Using valid Minecraft installation"}));
+	        textField.setText(Settings.minecraftDirectory);
+		    isSelected = true;
+	    } else {
+	    	JFileChooser fileChooser = new JFileChooser();
+	    	fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	    	if (fileChooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+	    		File file = fileChooser.getSelectedFile();
+	    		Settings.minecraftDirectory = file.getAbsolutePath()+"/mods";
+	    	}
+	    	changeMode(0);
+	    }
+    }
+    
+    public static void loadTechnic()
+    {
+        if (Files.exists(Paths.get(Settings.technicDirectory))) {
+        	String[] directories = new File(Settings.technicDirectory).list(new FilenameFilter() {
+	     	  public boolean accept(File current, String name) {
+	        	return new File(current, name).isDirectory();
+	          }
+	        });
+		    comboBox_1.setModel(new DefaultComboBoxModel(directories));
+	    	textField.setText("Please choose a modpack");
+	    	isValid = true;
+	    } else {
+	    	textField.setText("Technic installation not found");
+	    	comboBox_1.setModel(new DefaultComboBoxModel(new String[]{"Please see error above!"}));
+	    	int result = JOptionPane.showConfirmDialog(null, "Technic not found, select directory?", null, JOptionPane.YES_NO_OPTION);
+	    	if (result == JOptionPane.YES_OPTION) {
+		    	JFileChooser fileChooser = new JFileChooser();
+		    	fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		    	if (fileChooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+		    		File file = fileChooser.getSelectedFile();
+		    		Settings.technicDirectory = file.getAbsolutePath()+"/modpacks";
+			    	changeMode(1);
+			    	return;
+		    	}
+	    	}
+	    	textField.setText("Invalid Technic installation");
+	    }
+    }
 }
---
+++ src/main/java/tech/tabpixels/pixelmod/mini/Settings.java
@@ -0,0 +1,19 @@
+package tech.tabpixels.pixelmod.mini;
+
+public class Settings {
+	public static Integer selection = 0;
+    public static String minecraftDirectory = "";
+    public static String technicDirectory = "";
+    public static String modpackDirectory = "";
+    public static String multimcDirectory = "";
+    
+    public static void load()
+    {
+    	
+    }
+	
+    public static void save()
+    {
+    	
+    }
+}
